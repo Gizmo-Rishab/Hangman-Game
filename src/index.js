d638a47771182417b32da9c16eb4f97120f071f2
Rishab Arora <rishabarora2008@gmail.com>
2019-11-13 21:59:43 +0530

    Added finishing touches Renaming to webpack boiler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 'use strict'
-const name = 'Rishab'
-console.log(name)
-
-class Hangman {
-	myMethod() {
-    	return 'Testing 123'
-    }
-}
-const hangman = new Hangman()
-console.log(hangman.myMethod())
